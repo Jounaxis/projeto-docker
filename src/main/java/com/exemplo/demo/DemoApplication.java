3975972e4ab9e3370ee78c6e6029b0d810748149
Jounaxis <jgs123098@gmail.com>
2026-04-28 21:11:09 -0300

    Atualização na aplicação para garantir o funcionamento coerente do CRUD

--- src/main/java/com/exemplo/demo/DemoApplication.java
+++ src/main/java/com/exemplo/demo/DemoApplication.java
@@ -1,50 +1,79 @@
 package com.exemplo.demo;
 
-import jakarta.persistence.*;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.persistence.*;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    // Inserindo seus dados automaticamente ao subir a aplicação
     @Bean
     public CommandLineRunner carregarDados(AlunoRepository repository) {
         return (args) -> {
             repository.save(new Aluno("Jonatan", "rm566998", "26/02/2006"));
         };
     }
-
-    @GetMapping("/alunos")
-    public List<Aluno> listarAlunos(AlunoRepository repository) {
-        return repository.findAll();
-    }
 }
 
+// 1. Classe Aluno com os atributos corretos
 @Entity
 class Aluno {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
-    public String nome;
-    public String rm;
-    public String dataNascimento;
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String rm;
+    private String dataNascimento;
+
     public Aluno() {}
+
     public Aluno(String nome, String rm, String dataNascimento) {
         this.nome = nome;
         this.rm = rm;
         this.dataNascimento = dataNascimento;
     }
+
+    // Getters e Setters obrigatórios para o Jackson (Postman) funcionar
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getRm() { return rm; }
+    public void setRm(String rm) { this.rm = rm; }
+    public String getDataNascimento() { return dataNascimento; }
+    public void setDataNascimento(String dataNascimento) { this.dataNascimento = dataNascimento; }
 }
 
-interface AlunoRepository extends JpaRepository<Aluno, Long> {}
+interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
+
+// 2. Controladores com GET e POST
+@RestController
+@RequestMapping("/alunos")
+class AlunoController {
+
+    @Autowired
+    private AlunoRepository repository;
+
+    @GetMapping
+    public List<Aluno> listarTodos() {
+        return repository.findAll();
+    }
+
+    // 3. O método que faltava para receber os dados do Postman!
+    @PostMapping
+    public Aluno criarAluno(@RequestBody Aluno aluno) {
+        return repository.save(aluno);
+    }
+}
